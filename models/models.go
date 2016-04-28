5df156b559dfbd52eb4581b82c7239d4356b32c9
skykewei <abc10086hi@126.com>
2016-04-28 21:03:51 +0800

    Three structs in models/models.go

--- models/models.go
+++ models/models.go
@@ -1,1 +1,36 @@
 package models
+
+import (
+	"time"
+
+	"gopkg.in/mgo.v2/bson"
+)
+
+type (
+	Owner struct {
+		Id           bson.ObjectId `bson:"_id,omitempty" json:"id"`
+		FirstName    string        `json:"firstname"`
+		LastName     string        `json:"lastname"`
+		Email        string        `json:"email"`
+		Password     string        `json:"password,omitempty"`
+		HashPassword []byte        `json:"hashpassword,omitempty"`
+	}
+	LogicalObject struct {
+		Id          bson.ObjectId `bson:"_id,omitempty" json:"id"`
+		CreatedBy   string        `json:"createdby"`
+		BucketName  string        `json:"bucketname"`
+		ObjectKey   string        `json:"objectkey"`
+		Description string        `json:"description"`
+		CreatedOn   time.Time     `json:"createdon,omitempty"`
+		Due         time.Time     `json:"due"`
+		Version     string        `json:"version,omitempty"`
+		Status      string        `json:"status,omitempty"`
+		Tags        []string      `json:"tags,omitempty"`
+	}
+	ObjectOwned struct {
+		Id              bson.ObjectId `bson:"_id,omitempty" json:"id"`
+		LogicalObjectId bson.ObjectId `json:"logicalobjectid"`
+		Description     string        `json:"description"`
+		CreatedOn       time.Time     `json:"createdon,omitempty"`
+	}
+)
